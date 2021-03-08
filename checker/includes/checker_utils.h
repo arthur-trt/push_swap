2a391daa77995a3639fd710f0f13e1d0394b902a
Arthur TROUILLET <atrouill@e1r7p8.42.fr>
2021-03-08 16:57:00 +0100

    Remove unused functions

--- checker/includes/checker_utils.h
+++ checker/includes/checker_utils.h
@@ -6,7 +6,7 @@
 /*   By: atrouill <atrouill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/08 15:39:06 by atrouill          #+#    #+#             */
-/*   Updated: 2021/03/08 16:12:07 by atrouill         ###   ########.fr       */
+/*   Updated: 2021/03/08 16:56:45 by atrouill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,5 @@
 # define CHECKER_UTILS_H
 
 int		ft_atoi(const char *str);
-char	**ft_split(char const *s, char c);
-void	free_splitted(char **split);
 
 #endif
