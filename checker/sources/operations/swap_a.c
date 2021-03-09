9b78d34eae2b72de4ceb164e55afebba6faf6f9d
arthur-trt <atrouill@student.42.fr>
2021-03-09 11:02:57 +0100

    Typo in code

--- checker/sources/operations/swap_a.c
+++ checker/sources/operations/swap_a.c
@@ -6,7 +6,7 @@
 /*   By: atrouill <atrouill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/08 15:14:31 by atrouill          #+#    #+#             */
-/*   Updated: 2021/03/08 19:35:31 by atrouill         ###   ########.fr       */
+/*   Updated: 2021/03/09 11:02:29 by atrouill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static bool	check_two_a_set(t_stack *stack)
 	t_stack	*tmp;
 	int		set;
 
-	tmp = stack;
+	tmp = find_first_set_a(stack);
 	set = 0;
 	while (tmp && tmp->a_set)
 	{
@@ -37,11 +37,16 @@ static bool	check_two_a_set(t_stack *stack)
 void	swap_a(t_stack **stack)
 {
 	int	tmp;
+	t_stack	*first_a;
 
+	#ifdef DEBUG
+		printf("\nswap a\n");
+	#endif
 	if (*stack && check_two_a_set(*stack))
 	{
-		tmp = (*stack)->next->a;
-		(*stack)->next->a = (*stack)->a;
-		(*stack)->a = tmp;
+		first_a = find_first_set_a(*stack);
+		tmp = first_a->next->a;
+		first_a->next->a = first_a->a;
+		first_a->a = tmp;
 	}
 }
