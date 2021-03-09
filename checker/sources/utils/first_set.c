8d1ff1bf95f4272a69e4fc3e35d7fe4f50ee0961
arthur-trt <atrouill@student.42.fr>
2021-03-09 11:03:29 +0100

    Add some utils fucntions

---
+++ checker/sources/utils/first_set.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   first_set.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atrouill <atrouill@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/08 23:13:05 by atrouill          #+#    #+#             */
+/*   Updated: 2021/03/08 23:56:28 by atrouill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "checker.h"
+
+/*
+**	Find the position of first num in a stack and return pointer to location
+**
+**	@param stack Linked list represent the stack
+**
+**	@return Pointer on first link where a is set
+*/
+t_stack	*find_first_set_a(t_stack *stack)
+{
+	t_stack	*tmp;
+
+	if (stack)
+	{
+		tmp = stack;
+		while (tmp && tmp->a_set == false)
+			tmp = tmp->next;
+		if (tmp && tmp->a_set)
+			return (tmp);
+	}
+	return (NULL);
+}
+
+/*
+**	Find the position of first num in b stack and return pointer to location
+**
+**	@param stack Linked list represent the stack
+**
+**	@return Pointer on first link where b is set
+*/
+t_stack	*find_first_set_b(t_stack *stack)
+{
+	t_stack	*tmp;
+
+	if (stack)
+	{
+		tmp = stack;
+		while (tmp && tmp->b_set == false)
+			tmp = tmp->next;
+		if (tmp && tmp->b_set)
+			return (tmp);
+	}
+	return (NULL);
+}
---
+++ checker/sources/utils/last_unset.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   last_unset.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atrouill <atrouill@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/08 23:20:03 by atrouill          #+#    #+#             */
+/*   Updated: 2021/03/08 23:39:14 by atrouill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "checker.h"
+
+/*
+**	Find the position of first num in a stack and return pointer to location
+**
+**	@param stack Linked list represent the stack
+**
+**	@return Pointer on first link where a is set
+*/
+t_stack	*find_last_unset_a(t_stack *stack)
+{
+	t_stack	*tmp;
+
+	if (stack)
+	{
+		tmp = stack;
+		if (tmp->a_set)
+			return (NULL);
+		while (tmp->next && tmp->next->a_set == false)
+			tmp = tmp->next;
+		if (tmp->a_set == false)
+			return (tmp);
+	}
+	return (NULL);
+}
+
+/*
+**	Find the position of first num in b stack and return pointer to location
+**
+**	@param stack Linked list represent the stack
+**
+**	@return Pointer on first link where b is set
+*/
+t_stack	*find_last_unset_b(t_stack *stack)
+{
+	t_stack	*tmp;
+
+	if (stack)
+	{
+		tmp = stack;
+		if (tmp->b_set)
+			return (NULL);
+		while (tmp->next && tmp->next->b_set == false)
+			tmp = tmp->next;
+		if (tmp->b_set == false)
+			return (tmp);
+	}
+	return (NULL);
+}
